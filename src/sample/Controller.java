7c473454ae060dba483fe84f5a150b53b6488fef
Shayne Lewis <60145924+shayne-lewis@users.noreply.github.com>
2021-04-05 15:58:15 -0400

    skeleton code for server and clients

---
+++ src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class Controller {
+    @FXML
+    public void initialize(){
+
+    }
+
+    @FXML
+    public void exitClient(){
+        System.exit(0);
+    }
+}
